70e21701204c47d548bc9025a3f0f4f192ebe1dd
Julio Lopez <joeyclopez88#gmail.com>
2021-08-07 14:11:38 -0700

    initial commit

---
+++ src/components/DogList.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+import './DogList.style.css';
+import { Link } from "react-router-dom";
+
+class DogList extends Component{
+    render() {
+        return (
+            <div className="DogList">
+                <h1 className="display-1 text-center my-5">Dog List!</h1>
+               
+                    <div className="row">
+                    {this.props.dogs.map( dog => (
+                        <div className="Dog col-md-6 col-lg-4 text-center" key={dog.name}>
+                            <img src={dog.src} alt={dog.name} />
+                           
+                            <h3 className="mt-5">
+                            <Link className="underline" to={`/dogs/${dog.name}`} >{dog.name}</Link>
+                            </h3>
+                          
+                           
+                        </div> 
+                        ) )}
+                       
+                    </div>
+            </div>
+        )
+    }
+}
+
+export default DogList;
